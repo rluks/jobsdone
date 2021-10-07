51cfab2439703706cbb18acaf6e375dc05692302
rluks <rluks+x@pm.me>
2021-10-07 09:52:33 +0200

    Resolution option id updated for Red Hat Jira

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 const audioPath = "assets/audio/jobs_done.mp3";
-const doneOptionId = "1";
-const doneButtonId = "issue-workflow-transition-submit";
 const resolutionId = "resolution";
+const resolutionOptionId = "10801";
+const doneButtonId = "issue-workflow-transition-submit";
 
 let jobsDoneAudio = new Audio();
 jobsDoneAudio.src = chrome.runtime.getURL(audioPath);
@@ -9,7 +9,7 @@ jobsDoneAudio.src = chrome.runtime.getURL(audioPath);
 document.addEventListener("click", function (e) {
     if (e.target && e.target.id == doneButtonId) {
         let resolution = document.getElementById(resolutionId);
-        if (resolution && resolution.options[resolution.selectedIndex].value == doneOptionId) {
+        if (resolution && resolution.options[resolution.selectedIndex].value == resolutionOptionId) {
             jobsDoneAudio.play();
         }
     }
